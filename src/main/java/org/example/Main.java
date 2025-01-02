a4738def209895d7975fa58f0a8e0cbc1cd2bc4e
Jenil <jenil.kajavadara1824@gmail.com>
2025-01-02 11:33:15 +0530

    output

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,13 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,7 +19,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
         double id=10000000000001L;
+
+        int flag_occurence=0;
+        String flag_occurences="";
         // Open the Excel file
+
         FileInputStream fis = new FileInputStream(new File("/home/jenil/Downloads/data.xlsx"));
         Workbook workbook = new XSSFWorkbook(fis);
         Sheet sheet = workbook.getSheetAt(3);
@@ -27,52 +34,83 @@ public class Main {
 
         JSONArray jsonArray = new JSONArray();
 
-        for (int i = 1; i < numberOfRows; i++) {
+        for (int i = 3; i < numberOfRows; i++) {
             Row row = sheet.getRow(i);
             JSONObject jsonObject = new JSONObject();
-                jsonObject.put("rule.name", row.getCell(3));
-                jsonObject.put("rule.catogary", "Network");
+            if(row.getCell(3).toString()!=""){
+                jsonObject.put("rule.name", row.getCell(3));}
+            jsonObject.put("rule.catogary", "Network");
                 //Rule context obj
-                JSONObject rule_context = new JSONObject();
-                    rule_context.put("rule.check.catagory", row.getCell(11));
-                    rule_context.put("rule.check.type", row.getCell(10));
+            JSONObject rule_context = new JSONObject();
+            if(row.getCell(11).toString()!="")
+                rule_context.put("rule.check.catagory", row.getCell(11));
+            if(row.getCell(10).toString()!=""){
+                rule_context.put("rule.check.type", row.getCell(10));}
                     JSONArray rule_conditions = new JSONArray(); //array for rule conditions
                         JSONObject condition = new JSONObject();
-                        condition.put("condition", row.getCell(12));
-                        condition.put("result.pattern", row.getCell(13));
+            if(row.getCell(12)!=null && row.getCell(12).toString()!="")
+                condition.put("condition", row.getCell(12));
+            if(row.getCell(12)!=null && row.getCell(13).toString()!=""){
+                String cellContent = row.getCell(13).toString(); // Get the cell value
+                cellContent = cellContent.replace("\n", "");    // Remove newlines
+                condition.put("result.pattern", cellContent);
+                condition.put("result.pattern", cellContent);}
                         if(row.getCell(14)!=null)
                         {
-                            if(row.getCell(14).toString() == "any")
-                                condition.put("occurence", -1);}
-//                          else
+                            if(row.getCell(14).toString().equals("any"))
+                                condition.put("occurence", -1);
+                        }
+//                            else
 //                              condition.put("occurence", row.getCell(14));
-                        condition.put("operator", row.getCell(15));
+            if(row.getCell(12)!=null && row.getCell(15).toString()!=""){
+                condition.put("operator", row.getCell(15));}
                         rule_conditions.put(condition);
                     rule_context.put("rule.conditions", rule_conditions);
                 jsonObject.put("rule_context", rule_context);
                 jsonObject.put("rule.auto.remediation", "no");
+            if(row.getCell(5).toString()!="")
                 jsonObject.put("rule.description", row.getCell(5));
-                jsonObject.put("rule.severity", row.getCell(17));
+            if(row.getCell(17).toString()!=""){
+                jsonObject.put("rule.severity", row.getCell(17));}
                 jsonObject.put("rule.tags", "tags");
+            if(row.getCell(6).toString()!="")
                 jsonObject.put("rule.rationale", row.getCell(6));
+            if(row.getCell(7).toString()!="")
                 jsonObject.put("rule.impact", row.getCell(7));
+            if(row.getCell(35).toString()!="")
                 jsonObject.put("rule.default.value", row.getCell(35));
+            if(row.getCell(34).toString()!="")
                 jsonObject.put("rule.references", row.getCell(34));
-                jsonObject.put("rule.additional.information", row.getCell(16));
+            if(row.getCell(16).toString()!=""){
+                jsonObject.put("rule.additional.information", row.getCell(16));}
                 //Array for rule control
                 JSONArray rule_controls = new JSONArray();
                 JSONObject control1 = new JSONObject();//control 1 obj
-                    control1.put("rule.control.version", "v"+row.getCell(22));
+
+            String rule_control_version = row.getCell(18).toString();
+            Pattern versionPattern = Pattern.compile("CONTROL:(.*?)(?=DESCRIPTION:)");
+            String version1;
+            Matcher versionMatcher = versionPattern.matcher(rule_control_version);
+            if (versionMatcher.find()) {
+                version1 = versionMatcher.group(1).trim();
+            }
+            else
+                version1="";
+
+            if(version1!=""){
+                control1.put("rule.control.version", version1);}
+
                         String input_title = row.getCell(18).toString();
                             String title;
-                            Pattern pattern_title = Pattern.compile("TITLE:(.*)");
+                            Pattern pattern_title = Pattern.compile("^TITLE:(.*)CONTROL:");
                             Matcher matcher_title = pattern_title.matcher(input_title);
                             if (matcher_title.find()) {
-                                title = matcher_title.group(1).trim(); // Extract and trim the result
+                                title = matcher_title.group(1).trim();
                             }
                             else
                                 title = "";
-                    control1.put("rule.control.name", title);
+                            if(title!="")
+                                control1.put("rule.control.name", title);
 
                         String input1 = row.getCell(18).toString();
                         String description1;
@@ -83,35 +121,57 @@ public class Main {
                         }
                         else
                             description1 = "";
-                        control1.put("rule.control.description", description1);
+                        if(description1!="")
+                            control1.put("rule.control.description", description1);
 
-                        String[] rules_ig1 = {"", "", ""};
+
+                        List<String> rules_ig1 = new ArrayList<>();
+
                         if (row.getCell(25).toString() != "" ) {
-                            rules_ig1[0] = "ig1";
+                            //rules_ig1[0] = "ig1";
+                            rules_ig1.add("ig1");
+
                         }
                         if (row.getCell(26).toString() != "" ) {
-                            rules_ig1[1] = "ig2";
+                            //rules_ig1[1] = "ig2";
+                            rules_ig1.add("ig2");
                         }
                         if (row.getCell(27).toString() != "" ) {
-                            rules_ig1[2] = "ig3";
+                            //rules_ig1[2] = "ig3";
+                            rules_ig1.add("ig3");
                         }
-                    control1.put("rule.control.ig", rules_ig1);
+                        if(!rules_ig1.isEmpty())
+                            control1.put("rule.control.ig", rules_ig1);
 
                 JSONObject control2 = new JSONObject();//control 2 obj
-                    control2.put("rule.control.version", "v"+row.getCell(28));
+            String rule_control_version2 = row.getCell(19).toString();
+            Pattern versionPattern2 = Pattern.compile("CONTROL:(.*?)(?=DESCRIPTION:)");
+            String version2;
+            Matcher versionMatcher2 = versionPattern2.matcher(rule_control_version2);
+            if (versionMatcher2.find()) {
+                version2 = versionMatcher2.group(1).trim();
+            }
+            else
+                version2="";
 
-                    String input_title2 = row.getCell(18).toString();
-                    String title2;
-                    Pattern pattern_title2 = Pattern.compile("TITLE:(.*)");
-                    Matcher matcher_title2 = pattern_title2.matcher(input_title2);
-                    if (matcher_title2.find()) {
-                        title2 = matcher_title.group(1).trim(); // Extract and trim the result
-                    }
-                    else
-                        title2 = "";
+            if(version2!=""){
+                control2.put("rule.control.version", version2);}
 
-                    control2.put("rule.control.name", title2);
-                    control2.put("rule.control.name", title2);
+            String input_title2 = row.getCell(19).toString();
+            String title2;
+
+            Pattern pattern_title2 = Pattern.compile("^TITLE:(.*)CONTROL:");
+            Matcher matcher_title2 = pattern_title2.matcher(input_title2);
+
+            if (matcher_title2.find()) {
+                title2 = matcher_title2.group(1).trim(); // Extract and trim the result
+            } else {
+                title2 = "";
+            }
+
+
+            if(title2!=""){
+                control2.put("rule.control.name", title2);}
                     String input = row.getCell(19).toString();
                     //extract description
                     String description;
@@ -122,18 +182,23 @@ public class Main {
                     }
                     else
                         description = "";
-                    control2.put("rule.control.description", description);
-                    String[] rules_ig2 = {"", "", ""};
+                    if(description!="")
+                        control2.put("rule.control.description", description);
+                       List<String> rules_ig2 = new ArrayList<>();
+                    StringBuffer sb2 = new StringBuffer("");
                     if (row.getCell(31).toString() != ""){
-                        rules_ig2[0] = "ig1";
+                        rules_ig2.add("ig1");
                     }
                     if (row.getCell(32).toString() != "" ) {
-                        rules_ig2[1] = "ig2";
+                        //rules_ig2[1] = "ig2";
+                        rules_ig2.add("ig2");
                     }
                     if (row.getCell(33).toString() != "" ) {
-                       rules_ig2[2] = "ig3";
+//                       rules_ig2[2] = "ig3";
+                        rules_ig2.add("ig3");
                     }
-                    control2.put("rule.control.ig", rules_ig2);
+                    if(!rules_ig2.isEmpty())
+                        control2.put("rule.control.ig", rules_ig2);
                 rule_controls.put(control1);
                 rule_controls.put(control2);
             jsonObject.put("rule.controls", rule_controls);
@@ -148,6 +213,13 @@ public class Main {
 //        String s = sheet.getRow(3).getCell(25).toString();
 //        System.out.println(s=="");
         //System.out.println("output "+s);
+//        flag_occurences=sheet.getRow(5).getCell(14).toString();
+//        if(flag_occurences.equals("any"))
+//        {
+//            System.out.println("any true if");
+//        }
+//        System.out.println(flag_occurence);
+//        System.out.println("Last :"+flag_occurences);
         workbook.close();
         fis.close();
     }
